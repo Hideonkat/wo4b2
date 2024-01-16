11b2c52dd3ac959cb1216b8139dd82311042c4a3
Hideonkat <152405985+Hideonkat@users.noreply.github.com>
2024-01-16 18:11:30 +0700

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,44 +1,37 @@
 ﻿
-
+namespace orderarry;
 internal class Program
 {
     private static void Main(string[] args)
     {
-        int size;
-            int[] array;
-            do
-            {
-                Console.WriteLine("Enter a size:");
-                size = Int32.Parse(Console.ReadLine());
-                if (size > 20)
-                    Console.WriteLine("Size should not exceed 20");
-            } while (size > 20);
-
-            array = new int[size];
-            int i = 0;
-            while (i < array.Length)
-            {
-                Console.WriteLine("Enter element" + (i + 1) + " : ");
-                array[i] = Int32.Parse(Console.ReadLine());
-                i++;
-            }
-
-            Console.WriteLine("Property list: ");
-            for (int j = 0; j < array.Length; j++)
-            {
-                Console.WriteLine(array[j] + "\t");
-            }
-
-            int max = array[0];
-            int index = 1;
-            for (int j = 0; j < array.Length; j++)
-            {
-                if (array[j] > max)
-                {
-                    max = array[j];
-                    index = j + 1;
-                }
-            }
-            Console.WriteLine("The largest property value in the list is " + max + " ,at position " + index);
-}
+        int a;
+        Console.WriteLine("nhap so phan tu mang:");
+        a = Int32.Parse(Console.ReadLine());
+        int[] mang = new int[a+1];
+        for (int i = 0; i < a; i++)
+        {
+            Console.WriteLine("Nhap phan tu mang:");
+            mang[i] = int.Parse(Console.ReadLine());
+        }
+        Console.WriteLine("Mang co cac phan tu la:");
+        for (int i = 0; i < a; i++)
+        {
+            Console.Write(mang[i]+"\t");
+        }
+        Console.Write("\n");
+        Console.WriteLine("nhap phan tu can chen:");
+        int b = int.Parse(Console.ReadLine());
+        Console.WriteLine("nhap vi tri chen:");
+        int c = int.Parse(Console.ReadLine());
+        for (int i = a; i>=c ; i--)
+        {
+            mang[i]=mang[i-1];
+        }
+        mang[c-1]=b; //chen phan tu
+        for (int i = 0; i <= a; i++)
+        {
+            Console.Write(mang[i]+"\t");
+        }
+        Console.WriteLine("ok");
+        }
 }
